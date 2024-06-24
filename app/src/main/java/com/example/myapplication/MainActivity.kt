912fe421a9f3a361b6d0c84f640803d75862755c
ROSHAN DANIEL RAJ <1668232roshandanielraj@gmail.com>
2024-06-24 10:48:57 +0530

    Button click

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +19,9 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun myclickhandler(view: View) {
+        Log.i("MainActivity","Button clicked")
+    }
+
 }
